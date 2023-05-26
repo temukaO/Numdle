83167f7887e93f12e2ade8b88fc4208b8a85bceb
Temuujin Otgonbayar <t.otgonbayar.o@gmail.com>
2023-05-26 01:10:20 -0700

    Fixed undefined error

--- main.js
+++ main.js
@@ -1,31 +1,34 @@
 let guessCounter = 0;
 rndmNumbers();
-function rndmNumbers(){
+function rndmNumbers(userGuess,count){
     const numbers = [];
     for(let i = 0;i<5;i++){
         const number = Math.floor(Math.random() * (9 - 0 + 1)) + 0;
         numbers.push(number);
     }
     console.log(numbers);
-    guess(numbers);
+    verify(numbers,userGuess,count);
+    
 }
-function guess(nums){
+function guess(){
     guessCounter++;
-    let guesses = [];
     let guess = document.getElementById('guess').value;
     guess = guess.replace(/ /g, "");
-    if(guess!=null){
-        for(let i = 0; i<guess.length;i++){
-            guesses[i] = guess[i];
-            document.getElementById('r'+guessCounter+'box'+i).innerHTML = guesses[i];
-            if(nums[i] === parseInt(guesses[i])){
-                const elementCorrect = document.getElementById('r'+guessCounter+'box'+i);
-                elementCorrect.style.color = "green"; 
-            }
-            else if(nums[i] === parseInt(guesses[i])){
-                const elementCorrect = document.getElementById('r'+guessCounter+'box'+i);
-                elementCorrect.style.color = "red"; 
-            }
+    rndmNumbers(guess,guessCounter);
+}
+
+function verify(nums,guess,count){
+    let guesses = [];
+    for(let i = 0; i<5;i++){
+        guesses[i] = guess[i];
+        document.getElementById('r'+count+'box'+i).innerHTML = guesses[i];
+        if(nums[i] === parseInt(guesses[i])){
+            const elementCorrect = document.getElementById('r'+count+'box'+i);
+            elementCorrect.style.color = "green"; 
+        }
+        else if(nums[i] === parseInt(guesses[i])){
+            const elementCorrect = document.getElementById('r'+count+'box'+i);
+            elementCorrect.style.color = "red"; 
         }
     }
 }
