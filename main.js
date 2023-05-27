685a8e40a42050ef702ae53047223cea6e60dde8
Temuujin Otgonbayar <t.otgonbayar.o@gmail.com>
2023-05-27 10:33:04 -0700

    Added hint

--- main.js
+++ main.js
@@ -1,34 +1,40 @@
 let guessCounter = 0;
+const numbers = [];
 rndmNumbers();
-function rndmNumbers(userGuess,count){
-    const numbers = [];
+function rndmNumbers(){
     for(let i = 0;i<5;i++){
         const number = Math.floor(Math.random() * (9 - 0 + 1)) + 0;
         numbers.push(number);
     }
     console.log(numbers);
-    verify(numbers,userGuess,count);
     
 }
 function guess(){
     guessCounter++;
     let guess = document.getElementById('guess').value;
     guess = guess.replace(/ /g, "");
-    rndmNumbers(guess,guessCounter);
+    verify(numbers,guess,guessCounter);
 }
-
 function verify(nums,guess,count){
     let guesses = [];
+    let check = new Map();
+    let elementCorrect;
     for(let i = 0; i<5;i++){
         guesses[i] = guess[i];
         document.getElementById('r'+count+'box'+i).innerHTML = guesses[i];
-        if(nums[i] === parseInt(guesses[i])){
-            const elementCorrect = document.getElementById('r'+count+'box'+i);
-            elementCorrect.style.color = "green"; 
-        }
-        else if(nums[i] === parseInt(guesses[i])){
-            const elementCorrect = document.getElementById('r'+count+'box'+i);
-            elementCorrect.style.color = "red"; 
+        elementCorrect = document.getElementById('r'+count+'box'+i);
+        for(let j = 0;j<5;j++){
+            if(nums[i]===parseInt(guesses[j])){
+                elementCorrect.style.backgroundColor = 'green'; 
+            }
+            else if(nums.includes(parseInt(guess[i]))){
+                elementCorrect.style.backgroundColor = 'yellow';
+            } 
+            else{
+                elementCorrect.style.backgroundColor = 'red'; 
+            }
         }
     }
+}
+function winCheck() {
 }
